ddd61e41bd53172289663f43d4530c58ad6f82fa
Dan Joar Heimvik <danjoar@gmail.com>
2023-05-11 11:15:20 +0200

    added some resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,8 +1,65 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
 import React from 'react'
 
 function Resume() {
   return (
+    <div className="resume">
     <div>Resume</div>
+      <div>
+        <h1> Dan Joar Heimvik </h1>
+        <h2> Software developer </h2>
+      </div>
+
+      <div>
+        <h2> Education </h2>
+        <h3> Master's degree in Software development </h3>
+        <h4> University of Bergen / Western Norway University of Applied Sciences </h4>
+        <h4> 2022 - 2024 </h4>
+        <p> Currently pursuing a master's degree in Software development at UiB/HVL </p>
+        <h3> Bachelor's degree in Cybersecurity </h3>
+        <h4> University of Bergen </h4>
+        <h4> 2019 - 2022 </h4>
+        <p> Bachelor's degree in Cybersecurity from UiB </p>
+      </div>
+
+    <div>
+      <h2> Work experience </h2>
+      <h3> Process operator (summer intern/on-call employee) - Hydro Husnes </h3>
+      <h4> 2017 - 2023 </h4>
+      <p> Working as a process operator at Hydro Husnes aluminium plant. Tasks involved shift-work and operating heavy machinery </p>
+    </div>
+
+    <div>
+      <h2> Skills </h2>
+      <h3> Programming languages </h3>
+      <h4> Most experience: </h4>
+      <p> Java, Python, C# </p>
+      <h4> Some experience: </h4>
+      <p> C, C++, JavaScript, HTML, CSS, SQL </p>
+      <h3> Frameworks </h3>
+      <h4> Most experience: </h4>
+      <p> .NET, Spring Boot, React </p>
+      <h4> Some experience: </h4>
+      <p> Flask, Django, AngularJS, React Native, Vue </p>
+      <h3> Other </h3>
+      <p> Git, Docker, Linux, Windows</p>
+    </div>
+
+    <div>
+      <h2> Interests </h2>
+      <p> Programming, electronics, gaming, music production, hiking, skiing </p>
+    </div>
+
+    <div>
+      <h2> Languages </h2>
+      <h3> Norwegian </h3>
+      <p> Native language </p>
+      <h3> English </h3>
+      <p> Fluent </p>
+    </div>
+
+    </div>
+
   )
 }
 
