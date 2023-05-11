1b501e7154c8867d44bf7144678f706f099090ba
Dan Joar Heimvik <danjoar@gmail.com>
2023-05-11 13:34:21 +0200

    added resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,64 +1,62 @@
-/* eslint-disable react/jsx-no-comment-textnodes */
 import React from 'react'
+import "../styles/Resume.css";
 
 function Resume() {
   return (
     <div className="resume">
-    <div>Resume</div>
-      <div>
-        <h1> Dan Joar Heimvik </h1>
-        <h2> Software developer </h2>
+      <div class="terminal">
+        <div class="output">
+          <span class="prompt">{'>'} </span><span class="command">cd Documents/</span><br></br>
+          <span class="prompt">{'>'} </span><span class="command">ls -l</span><br></br>
+          <span class="line">total 1</span><br></br>
+          <span class="line">-rw-r--r--  1 user  group  120 May 11 14:15 <span class="out">resume.txt</span></span><br></br>
+          <span class="prompt">{'>'} </span><span class="command">head -n 2 <span class="out">resume.txt</span></span><br></br>
+          <span class="line">Dan Joar Heimvik</span><br></br>
+          <span class="line">Software developer</span><br></br>
+          <span class="prompt">{'>'} </span><span class="command">cat <span class="out">resume.txt</span> | grep <span class="string">"Education"</span></span><br></br>
+          <span class="line">--== Education ==--</span><br></br>
+          <span class="line">Master's degree in Software Development</span><br></br>
+          <span class="line">University of Bergen / Western Norway University of Applied Sciences</span><br></br>
+          <span class="line">2022 - 2024</span><br></br>
+          <span class="line">...</span><br></br>
+          <span class="line">Bachelor's degree in Cybersecurity</span><br></br>
+          <span class="line">University of Bergen</span><br></br>
+          <span class="line">2019 - 2022</span><br></br>
+          <span class="prompt">{'>'} </span><span class="command">cat <span class="out">resume.txt</span> | grep <span class="string">"Work experience"</span></span><br></br>
+          <span class="line">--== Work experience ==--</span><br></br>
+          <span class="line">Process operator (summer intern/on-call employee) - Hydro Husnes</span><br></br>
+          <span class="line">2017 - 2023</span><br></br>
+          <span class="line">...</span><br></br>
+          <span class="prompt">{'>'} </span><span class="command">cat <span class="out">resume.txt</span> | grep <span class="string">"Skills"</span></span><br></br>
+          <span class="line">--== Skills ==--</span><br></br>
+          <span class="line">-- Programming languages --</span><br></br>
+          <span class="line">- Most experience:</span><br></br>
+          <span class="line">Java, Python, C#</span><br></br>
+          <span class="line">- Some experience:</span><br></br>
+          <span class="line">C, C++, JavaScript, HTML, CSS, SQL</span><br></br>
+          <span class="line">...</span><br></br>
+          <span class="line">-- Frameworks --</span><br></br>
+          <span class="line">Most experience:</span><br></br>
+          <span class="line">.NET, Spring Boot, React</span><br></br>
+          <span class="line">...</span><br></br>
+          <span class="line">Some experience:</span><br></br>
+          <span class="line">Flask, Django, AngularJS, React Native, Vue</span><br></br>
+          <span class="line">...</span><br></br>
+          <span class="line">Other</span><br></br>
+          <span class="line">Git, Docker, Linux, Windows, Static and dynamic code analysis</span><br></br>
+          <span class="prompt">{'>'} </span><span class="command">cat <span class="out">resume.txt</span> | grep <span class="string">"Interests"</span></span><br></br>
+          <span class="line">--== Interests ==--</span><br></br>
+          <span class="line">Programming, electronics, gaming, music production, hiking, skiing</span><br></br>
+          <span class="prompt">{'>'} </span><span class="command">cat <span class="out">resume.txt</span> | grep <span class="string">"Languages"</span></span><br></br>
+          <span class="line">--== Languages ==--</span><br></br>
+          <span class="line">Norwegian - Native</span><br></br>
+          <span class="line">English - Fluent</span><br></br>
+          <span class="prompt">{'>'} </span><span class="command">exit</span><br></br>
+        </div>
       </div>
-
-      <div>
-        <h2> Education </h2>
-        <h3> Master's degree in Software development </h3>
-        <h4> University of Bergen / Western Norway University of Applied Sciences </h4>
-        <h4> 2022 - 2024 </h4>
-        <p> Currently pursuing a master's degree in Software development at UiB/HVL </p>
-        <h3> Bachelor's degree in Cybersecurity </h3>
-        <h4> University of Bergen </h4>
-        <h4> 2019 - 2022 </h4>
-        <p> Bachelor's degree in Cybersecurity from UiB </p>
-      </div>
-
-    <div>
-      <h2> Work experience </h2>
-      <h3> Process operator (summer intern/on-call employee) - Hydro Husnes </h3>
-      <h4> 2017 - 2023 </h4>
-      <p> Working as a process operator at Hydro Husnes aluminium plant. Tasks involved shift-work and operating heavy machinery </p>
     </div>
 
-    <div>
-      <h2> Skills </h2>
-      <h3> Programming languages </h3>
-      <h4> Most experience: </h4>
-      <p> Java, Python, C# </p>
-      <h4> Some experience: </h4>
-      <p> C, C++, JavaScript, HTML, CSS, SQL </p>
-      <h3> Frameworks </h3>
-      <h4> Most experience: </h4>
-      <p> .NET, Spring Boot, React </p>
-      <h4> Some experience: </h4>
-      <p> Flask, Django, AngularJS, React Native, Vue </p>
-      <h3> Other </h3>
-      <p> Git, Docker, Linux, Windows</p>
-    </div>
-
-    <div>
-      <h2> Interests </h2>
-      <p> Programming, electronics, gaming, music production, hiking, skiing </p>
-    </div>
-
-    <div>
-      <h2> Languages </h2>
-      <h3> Norwegian </h3>
-      <p> Native language </p>
-      <h3> English </h3>
-      <p> Fluent </p>
-    </div>
-
-    </div>
+    
 
   )
 }
